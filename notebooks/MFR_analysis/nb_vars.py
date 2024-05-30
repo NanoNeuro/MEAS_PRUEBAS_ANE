b7887c427296fbf80aef9c2903d9dd3c530ef466
alexmascension <alexmascension@gmail.com>
2024-05-30 17:48:07 +0200

    Reformat the notebooks to split the parts of the analysis

---
+++ notebooks/MFR_analysis/nb_vars.py
@@ -0,0 +1,4 @@
+FOLDER_INPUT_BASE = f'/data/Proyectos/Nanoneuro_exps_ane/raw_files/2024_05_07/'
+FOLDER_OUTPUT_BASE = f'/data/Proyectos/Nanoneuro_exps_ane/results/MFR-Bursts-SI Analysis pre-post/'
+
+CUTOFF_SPIKES_HZ = 4
