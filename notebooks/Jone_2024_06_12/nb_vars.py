5cacaa0431c9cb119175fc5742f8f300864eba39
alexmascension <alexmascension@gmail.com>
2024-07-05 17:54:23 +0200

    Added nbs for Jone_2024_06_12

---
+++ notebooks/Jone_2024_06_12/nb_vars.py
@@ -0,0 +1,4 @@
+FOLDER_INPUT_BASE = f'/data/Proyectos/Nanoneuro_exps_MEAs/raw_files/Jone_2024_06_12/'
+FOLDER_OUTPUT_BASE = f'/data/Proyectos/Nanoneuro_exps_MEAs/results/Jone_2024_06_12/'
+
+CUTOFF_SPIKES_HZ = 0
