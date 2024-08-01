28f11ff8e643e139ed46b2cf7da5a88913c099f5
alexmascension <alexmascension@gmail.com>
2024-08-01 10:44:25 +0200

    Minor changes

--- notebooks/MFR_analysis/nb_vars.py
+++ notebooks/MFR_analysis/nb_vars.py
@@ -1,4 +1,4 @@
-FOLDER_INPUT_BASE = f'Z:/@Working/MEAS_PRUEBAS_ANE/raw_files/2024_06_07/'
-FOLDER_OUTPUT_BASE = f'Z:/@Working/MEAS_PRUEBAS_ANE/results/2024_06_07/'
+FOLDER_INPUT_BASE = f'/data/Proyectos/Nanoneuro_exps_MEAs/raw_files/Ane_2024_07_08/'
+FOLDER_OUTPUT_BASE = f'/data/Proyectos/Nanoneuro_exps_MEAs/results/Ane_2024_07_08/'
 
 CUTOFF_SPIKES_HZ = 4
