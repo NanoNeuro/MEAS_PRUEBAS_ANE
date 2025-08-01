6fddc34baaf1990068a5b84afed64dbcf7617d40
alexmascension <alexmascension@gmail.com>
2025-08-01 15:10:54 +0200

    Added Jone_20250304

---
+++ notebooks/Jone_20250304/.ipynb_checkpoints/nb_vars-checkpoint.py
@@ -0,0 +1,5 @@
+FOLDER_INPUT_BASE = f'/media/user/6CA0B160A0B13206/Proyectos/Nanoneuro_exps_MEAs/data/Jone_20240612/COMPARISON'
+FOLDER_INTERMEDIATE = f'/media/user/6CA0B160A0B13206/Proyectos/Nanoneuro_exps_MEAs/intermediate_files/Jone_20240612'
+FOLDER_OUTPUT = f'/media/user/6CA0B160A0B13206/Proyectos/Nanoneuro_exps_MEAs/results/Jone_20240612'
+
+CUTOFF_SPIKES_HZ = 0
---
+++ notebooks/Jone_20250304/nb_vars.py
@@ -0,0 +1,6 @@
+BASE = f"/mnt/data/Proyectos/Nanoneuro_exps_MEAs"
+FOLDER_INPUT_BASE = f'{BASE}/data/Jone_20250304/'
+FOLDER_INTERMEDIATE = f'{BASE}/intermediate_files/Jone_20250304'
+FOLDER_OUTPUT = f'{BASE}/results/Jone_20250304'
+
+CUTOFF_SPIKES_HZ = 1
