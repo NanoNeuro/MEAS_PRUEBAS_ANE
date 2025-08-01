2d019cb4f07dfec6663fd8e4e378183cefcf8635
alexmascension <alexmascension@gmail.com>
2025-08-01 15:09:59 +0200

    updated ANE_20240708

--- notebooks/Ane_20240708/COMPARISON/nb_vars.py
+++ notebooks/Ane_20240708/COMPARISON/nb_vars.py
@@ -1,4 +1,5 @@
 FOLDER_INPUT_BASE = f'/data/Proyectos/Nanoneuro_exps_MEAs/data/Ane_20240708/COMPARISON/raw'
 FOLDER_INTERMEDIATE = f'/data/Proyectos/Nanoneuro_exps_MEAs/intermediate_files/Ane_20240708'
+FOLDER_OUTPUT = f'/data/Proyectos/Nanoneuro_exps_MEAs/results/Ane_20240708'
 
 CUTOFF_SPIKES_HZ = 4
